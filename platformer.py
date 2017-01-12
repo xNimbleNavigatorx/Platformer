99b3ce5217b569aaea7058d1f7f8343115f63641
xNimbleNavigatorx <alexandrehankel@hanovernorwichschools.org>
2017-01-12 13:36:47 -0500

    Updated from Brython Server: 1/12/2017 1:36:46 PM

--- platformer.py
+++ platformer.py
@@ -42,52 +42,79 @@ class floor(Sprite):
     
     grnline = RectangleAsset(5, 50, dgreenline, dgreen)
     
-    def __init__
-    grnwave((100, 546))
-    grnwave((200, 546))
-    grnwave((300, 546))
-    grnwave((400, 546))
-    grnwave((500, 546))
-    grnwave((600, 546))
-    grnwave((700, 546))
-    grnwave((800, 546))
-    grnwave((900, 546))
-    grnwave((1000, 546))
-    grnwave((1100, 546))
-    grnwave((1200, 546))
-
-def grnwave(Sprite):
-    grnwave.dir = -1.5
+    def __init__(self, position, invx, invy):
+        super().__init__(floor.grnline, position)
+        self.vx = 5
+        self.vy = 5
+        self.vr = 0
+        
+    
+    def step(self):
+        self.x += self.vx
+        self.y += self.vy
+        self.rotation += self.vr
+        self.vr = .1
+        if self.y >= 600 or self.y <= 0:
+                self.vy = self.vy*-1
+                
+        if self.x >= 1200 or self.x <= 0:
+            self.vx = self.vx*-1
 
 class skipper(Sprite):
-    def __init__(self, position):
+    def __init__(self, position, invx, invy):
         super().__init__(position)
-        self.vx = 2
-        self.vy = 2
+        self.vx = invx
+        self.vy = invx
         self.vr = 0.01
     def step(self):
         self.x += self.vx
         self.y += self.vy
         myapp.listenKeyEvent('keydown', 'uparrow', upKey)
-    myapp.listenKeyEvent('keydown', 'leftarrow', leftKey)
-    myapp.listenKeyEvent('keydown', 'rightarrow', rightKey)
+        myapp.listenKeyEvent('keydown', 'leftarrow', leftKey)
+        sp.listenKeyEvent("keydown", "left arrow", self.left)
+        sp.listenKeyEvent("keydown", "left arrow", self.right)
         
+        self.x += -2
+        self.x += 2
+    
+
 skipper.go = True
-  
+
 def step():
     if skipper.go:
         skipper.x += skipper.vx
         skipper.y += skipper.vy
         if skipper.x < 0:
             pass
-            
+       
+class sp(App):
+    def __init__(self, position, invx, invy):
+        super().__init__(position)
+        self.vx = invx
+        self.vy = invx
+        
+        self.x += self.vx
+        self.y += self.vy
+        
+    sp.g1 = floor((100,550))
+    sp.g2 = floor((200,550))
+    sp.g3 = floor((300,550))
+    sp.g4 = floor((400,550))
+    sp.g5 = floor((500,550))
+    sp.g6 = floor((600,550))
+    sp.g7 = floor((700,550))
+    sp.g8 = floor((800,550))
+    sp.g9 = floor((900,550))
+    sp.g10 = floor((1000,550))
+    sp.g11 = floor((1100,550))
+    sp.g12 = floor((1200,550))
 
 
 def reverseKey(event):
     reverse(skipper)
 
 def upKey(event):
-    self.vy += 3
+    self.vy += -2 
     
 def leftKey(event):
     self.vx -= 2
@@ -95,7 +122,6 @@ def leftKey(event):
 
 def rightKey(event):
     self.vy += 2 
-
 
 
 
